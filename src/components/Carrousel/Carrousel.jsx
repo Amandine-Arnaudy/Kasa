b48668e45f35c101e21dda4636d01845e8b3d795
Amandine <amandine.arnaudy11@gmail.com>
2023-06-30 11:37:47 +0200

    Carrousel Done

--- src/components/Carrousel/Carrousel.jsx
+++ src/components/Carrousel/Carrousel.jsx
@@ -1,15 +1,57 @@
-import React from 'react'
-import '././carrousel.scss';
+import React, { useState } from 'react';
+import arrowLeft from '../../assets/arrow_left.png';
+import './carrousel.scss';
 
-function Carrousel(props) {
+function Carrousel({ pictures }) {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const goToPreviousImage = () => {
+    if (currentImageIndex === 0) {
+      setCurrentImageIndex(pictures.length - 1);
+    } else {
+      setCurrentImageIndex(currentImageIndex - 1);
+    }
+  };
+
+  const goToNextImage = () => {
+    if (currentImageIndex === pictures.length - 1) {
+      setCurrentImageIndex(0);
+    } else {
+      setCurrentImageIndex(currentImageIndex + 1);
+    }
+  };
+
   return (
     <>
       <div className='carrousel'>
-        {props.pictures.map((picture) => <img key={picture} src={picture} alt="images carrousel" />)}
-      </div>
+        {pictures.length > 1 && (
+          <img
+            className='previous_button'
+            src={arrowLeft}
+            alt='flèche'
+            onClick={goToPreviousImage}
+          />
+        )}
 
+        <img src={pictures[currentImageIndex]} alt='images carrousel' />
+
+        {pictures.length > 1 && (
+          <img
+            className='next_button'
+            src={arrowLeft}
+            alt='flèche'
+            onClick={goToNextImage}
+          />
+        )}
+
+        {pictures.length > 1 && (
+          <div className='image_counter'>
+            {currentImageIndex + 1}/{pictures.length}
+          </div>
+        )}
+      </div>
     </>
-  )
+  );
 }
 
-export default Carrousel
+export default Carrousel;
