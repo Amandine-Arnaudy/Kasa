e1b8758eddf9089d56c100f9a20eddc7b6f05dd7
Amandine <amandine.arnaudy11@gmail.com>
2023-07-18 20:23:12 +0200

    About Page

--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header/NavBar";
 import Home from "./pages/HomePage";
+import About from "./pages/About";
 import Footer from "./components/Footer/Footer";
 
 
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
       </Routes>
       <Footer />
     </div>
---
+++ src/pages/About.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import AboutBanner from '../components/About/Banner2/About'
+import Collapse from '../components/Collapse/Collapse'
+
+function About() {
+
+    const details = [
+        {
+            id: 0,
+            title: "Fiabilité",
+            text: "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes."
+        },
+        {
+            id: 1,
+            title: "Respect",
+            text: "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+        },
+        {
+            id: 2,
+            title: "Service",
+            text: "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."
+        },
+        {
+            id: 3,
+            title: "Sécurité",
+            text: "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
+        }
+    ];
+
+    return (
+        <>
+            <AboutBanner />
+            <section className='collapse'>
+                {details.map(details => {
+                    return (
+                        <div key={details.id} className="collapse_container">
+                            <Collapse title={details.title} text={details.text} />
+                        </div>
+                    )
+                })}
+            </section>
+        </>
+    )
+}
+
+export default About
